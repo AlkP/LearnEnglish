a81673c59c9d9f64bb1e8aa569fa37b61ee8fa41
alkp <apasenko@gmail.com>
2018-02-03 20:50:21 +0300

    Создание пользователей и связей

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
-User.create!(phone: '79781234567', role: 1, gender: 0, password: '123456', password_confirmation: '123456',
+user1 = User.create!(phone: '79781234567', role: 1, gender: 0, password: '123456', password_confirmation: '123456',
              telegram_id: 123456789, username: 'alkpas', first_name: 'Alexey', last_name: 'Pasenko')
 
-User.create!(phone: '79781234568', role: 0, gender: 0, password: '123456', password_confirmation: '123456',
+user2 = User.create!(phone: '79781234568', role: 0, gender: 0, password: '123456', password_confirmation: '123456',
              telegram_id: 123456782, username: 'Tanya')
 
-User.create!(phone: '79781234561', role: 0, gender: 0, password: '123456', password_confirmation: '123456',
+user3 = User.create!(phone: '79781234561', role: 0, gender: 0, password: '123456', password_confirmation: '123456',
              telegram_id: 123456788, username: 'Vova')
 
-Family.create!(parent_id: 1, child_id: 3)
-Family.create!(parent_id: 2, child_id: 3)
+Family.create!(parent_id: user1.id, child_id: user3.id)
+Family.create!(parent_id: user2.id, child_id: user3.id)
