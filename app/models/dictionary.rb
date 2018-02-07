48afa7ad739652a33464cd3b9993e130a891edf9
alkp <apasenko@gmail.com>
2018-02-07 22:01:56 +0300

    Добавляем словарь

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,4 +1,5 @@
 class Dictionary < ApplicationRecord
-  enum language:  %i[english german]
-  enum category:  %i[common people family job]
+  enum language:    %i[english german]
+  enum vocabulary:  %i[1000 3000]
+  enum category:    %i[common people family job]
 end
